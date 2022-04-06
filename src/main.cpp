692fe51ef914bc4c4e0203dad07fe38df48c217e
unknown <nathnaelmek@gmail.com>
2022-04-06 13:47:29 -0500

    add doorLatch()

--- src/main.cpp
+++ src/main.cpp
@@ -5,33 +5,55 @@
 
 
 
-int sensorOutside;
-int sensorInside;
+
+bool isLatchOpen[] = {false, false}; // stores the status of the latches, are they open or closed
+
+
+
+// locks/ unlocks the specified door lock
+// parameter lock specifies which servo latch is locked
+//parameter action specifies whether the action required is 
+// provides the ability to open/lock door from only one side or bothsides
+// action = 1 means lock
+//actions  = 0 means unlock
+void doorLatch(char lock, int action){
  
+  switch(lock){
+    
+    case 'o' : // outside latch
+      if(action)
+        Serial.println("locked outside latch");
+      else
+        Serial.println("unlocked outside latch");
+      break;
+
+
+    case 'i' : //inside latch
+      if(action)
+        Serial.println("locked inside latch");
+      else
+        Serial.println("unlocked inside latch");
+      break;
+
+    case 'b' : //both latches
+      if(action)
+        Serial.println("locked both latches");
+      else
+        Serial.println("unlocked both latches");
+      break;
+
+  }
+    Serial.println("Ulocked  outside latch");
+  
+
+}
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
 
-}
-
-// opens the specified door lock
-// parameter lock specifies which servo latch is locked
-// provides the ability to open/lock door from only one side or bothsides
-// lock = -1 ==> open the outside  the outside 
-void openDoor(int lock){
- 
-  Serial.println("open door");
-
-}
-
-
-// opens the specified door lock
-// parameter lock specifies which servo latch is locked
-// provides the ability to open/lock door from only one side or bothsides
-void closeDoor(int lock){
-  // counterclock wise
-  Serial.println("close door");
+  //start the latches in a locked position
+  doorLatch('b', 1);
 
 }
 
